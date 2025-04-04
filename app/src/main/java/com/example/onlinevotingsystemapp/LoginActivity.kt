dbf2dbad5def2605c818e7e40e2471ed59fac05f
unknown <kartiksharm242001@gmail.com>
2025-04-04 08:45:32 +0100

    added login activity

---
+++ app/src/main/java/com/example/onlinevotingsystemapp/LoginActivity.kt
@@ -0,0 +1,66 @@
+package com.example.onlinevotingsystemapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailET: EditText
+    private lateinit var passwordET: EditText
+    private lateinit var loginBtn: Button
+    private lateinit var signupTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        emailET = findViewById(R.id.et_email)
+        passwordET = findViewById(R.id.et_password)
+        loginBtn = findViewById(R.id.btn_login)
+        signupTV = findViewById(R.id.tv_register)
+
+        loginBtn.setOnClickListener {
+            val email = emailET.text.toString().trim()
+            val password = passwordET.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "All fields required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Login Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        signupTV.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
